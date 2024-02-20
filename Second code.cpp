381923b681fbae54991a0e34ae046fd8bc504a56
AndrewUrus117 <93611789+Krane117@users.noreply.github.com>
2024-02-20 23:47:26 +0100

    Add files via upload

---
+++ Second code.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    float annualSalary;
+    float monthSalary = annualSalary / 12;
+    cout << "Your monthly salary is " << monthSalary;
+
+    system("pause>0");
+}
